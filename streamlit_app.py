94ee46c221fe81039e31b0a7e450722d11b67ab3
omorasnow <140661138+omorasnow@users.noreply.github.com>
2023-07-26 16:45:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom's New Healthy Diner')
 streamlit.header('BreakFast Favorites')
 streamlit.text('🥣 Omega 3 & blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinash & Rocjet Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+streamlit.text('🥑 🍞 Avocado Toast')
